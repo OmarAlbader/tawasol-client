cd50caaad6e2ed3daefbf79077f0dc8ee59d82c5
OmarAlbader <omaratheer233@gmail.com>
2024-07-25 03:02:51 +0300

    change serverUrl to heroku commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const serverUrl = "http://localhost:4000";
+// export const serverUrl = "http://localhost:4000";
 // export const serverUrl = "https://tawasol-server-production.up.railway.app";
-// export const serverUrl = "https://tawasol-server1-c7849c46370a.herokuapp.com";
+export const serverUrl = "https://tawasol-server1-c7849c46370a.herokuapp.com";
 
 export const api = axios.create({
   baseURL: `${serverUrl}/api`,
